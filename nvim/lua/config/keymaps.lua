b611f0bb4c02f917e3675e1245be18905b046102
yuki-0224-ui <sannomiya0224@gmail.com>
2024-10-18 16:36:49 +0900

    add: w3m.vim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -36,7 +36,9 @@ end, opts)
 -- keymap('n', '<C-k>', '<cmd>bnext<CR>')
 keymap("t", "<Esc>", [[<C-\><C-n>]])
 keymap("n", "np", "<cmd>NoNeckPain<CR>", opts)
-keymap("n", "df", "<cmd>call deol#start({ 'cwd': '%'->expand()->fnamemodify(':h'), 'split': 'floating', 'floating_border': 'rounded'})<CR>", opts)
+keymap("n", "df",
+	"<cmd>call deol#start({ 'cwd': '%'->expand()->fnamemodify(':h'), 'split': 'floating', 'floating_border': 'rounded'})<CR>",
+	opts)
 keymap("n", "db", "<cmd>call deol#start({ 'cwd': '%'->expand()->fnamemodify(':h') })<CR>", opts)
 keymap('x', 'p', 'P', opts)
 keymap('x', 'y', 'mzy`z', opts)
@@ -44,6 +46,26 @@ keymap('x', '<', '<gv', opts)
 keymap('x', '>', '>gv', opts)
 keymap('n', 'U', '<C-r>', opts)
 keymap('n', 'ds', '<cmd>GitDiff<cr>', opts)
+
+-- w3m.vim
+keymap('n', '<leader>w', ':W3mVSplit google ', opts)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "w3m",
+	callback = function()
+		keymap('n', '<CR>', '<Plug>(w3m-click)', opts)
+		keymap('n', '<S-CR> ', '<Plug>(w3m-shift-click)', opts)
+		keymap('n', '<C-S-CR> ', '<Plug>(w3m-shift-ctrl-click)', opts)
+		keymap('n', '=', '<Plug>(w3m-show-link)', opts)
+		keymap('n', '/', '<Plug>(w3m-search-start)', opts)
+		keymap('n', '<m-d>', '<Plug>(w3m-address-bar)', opts)
+		keymap('n', '<leader>h', '<Plug>(w3m-back)', opts)
+		keymap('n', '<leader>l', '<Plug>(w3m-forward)', opts)
+		keymap('n', '<leader>s', '<Plug>(w3m-toggle-syntax)', opts)
+		keymap('n', '<leader>c', '<Plug>(w3m-toggle-use-cookie)', opts)
+		keymap('n', '<C-n>', '<Plug>(w3m-next-link)', opts)
+		keymap('n', '<C-p>', '<Plug>(w3m-prev-link)', opts)
+	end
+})
 
 -- test
 vim.keymap.set("n", "<leader>k", function() print("Hop!") end)
