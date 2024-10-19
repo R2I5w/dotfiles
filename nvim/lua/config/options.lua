a870e63d666ced936c8c52845ddfbb5d2c80e7e8
yuki-0224-ui <sannomiya0224@gmail.com>
2024-10-19 09:12:37 +0900

    add: vim.o.cmdheight = 0

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,7 @@ vim.wo.number = true
 vim.opt.laststatus = 3
 vim.opt.cursorline = true
 vim.opt.virtualedit = "none"
+vim.o.cmdheight = 0
 vim.cmd("set ignorecase")
 vim.cmd("set completeopt=menuone,noinsert")
 vim.cmd([[let maplocalleader = ' ']])
