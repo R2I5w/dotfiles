102a7ef41c97d6743070d9b2af4560e5b6d581e4
yuki-0224-ui <sannomiya0224@gmail.com>
2024-10-22 22:01:37 +0900

    add: lsp clangd

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,57 +8,58 @@ local capabilities = require("ddc_source_lsp").make_client_capabilities()
 local lspconfig = require("lspconfig")
 
 local servers = {
-	"lua_ls",
-	"taplo",
-	"yamlls",
-	"dockerls",
-	"pylsp",
+        "lua_ls",
+        "taplo",
+        "yamlls",
+        "dockerls",
+        "pylsp",
+        "clangd",
 }
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-	vim.lsp.handlers.hover,
-	{
-		border = "rounded", -- "shadow" , "none", "rounded"
-		-- border = border
-		-- width = 100,
-	}
+        vim.lsp.handlers.hover,
+        {
+                border = "rounded", -- "shadow" , "none", "rounded"
+                -- border = border
+                -- width = 100,
+        }
 )
 
 -- Auto start language servers.
 for _, name in ipairs(servers) do
-	lspconfig[name].setup({})
+        lspconfig[name].setup({})
 end
 
 -- LSP settings
 local function on_attach(client, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
 end
 
 -- Deno Language Server の設定
 lspconfig.denols.setup({
-	root_dir = function(fname)
-		local root = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", ".git")(fname)
-		return root or vim.fn.getcwd()
-	end,
-	init_options = {
-		lint = true,
-		unstable = true,
-		suggest = {
-			imports = {
-				hosts = {
-					["https://deno.land"] = true,
-					["https://x.nest.land"] = true,
-					["https://crux.land"] = true,
-				},
-			},
-		},
-	},
-	on_attach = on_attach,
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-	single_file_support = true,
+        root_dir = function(fname)
+                local root = lspconfig.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", ".git")(fname)
+                return root or vim.fn.getcwd()
+        end,
+        init_options = {
+                lint = true,
+                unstable = true,
+                suggest = {
+                        imports = {
+                                hosts = {
+                                        ["https://deno.land"] = true,
+                                        ["https://x.nest.land"] = true,
+                                        ["https://crux.land"] = true,
+                                },
+                        },
+                },
+        },
+        on_attach = on_attach,
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+        single_file_support = true,
 })
 
 vim.lsp.set_log_level("debug")
