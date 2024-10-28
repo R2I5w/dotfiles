8308346357a539df8f3da9203465400d1b0ead25
yuki-0224-ui <sannomiya0224@gmail.com>
2024-10-28 15:43:20 +0900

    add: ステータスラインの枠線

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,21 +1,21 @@
 -- colorscheme
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		vim.cmd("colorscheme gruvbox")
-	end
+        callback = function()
+                vim.cmd("colorscheme gruvbox")
+        end
 })
 
 -- Each time a file is opened, automatically navigate to the directory where the file resides
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function()
-		local dir = vim.fn.expand("%:p:h")
-		vim.cmd("silent! lcd " .. vim.fn.fnameescape(dir))
-	end
+        pattern = "*",
+        callback = function()
+                local dir = vim.fn.expand("%:p:h")
+                vim.cmd("silent! lcd " .. vim.fn.fnameescape(dir))
+        end
 })
 
 vim.api.nvim_create_user_command('GitDiff', function()
-	vim.cmd([[
+        vim.cmd([[
     new
     setlocal buftype=nofile bufhidden=delete noswapfile
     setfiletype gitcommit
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,6 +5,11 @@ vim.opt.cursorline = true
 vim.opt.virtualedit = "none"
 vim.o.cmdheight = 0
 vim.o.laststatus = 0
+vim.opt.fillchars = {
+        stl = '─',
+        stlnc = '─',
+}
+vim.opt.statusline = '─'
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
 vim.opt.breakindent = true
